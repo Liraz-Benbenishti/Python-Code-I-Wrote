8bcbe980cc81cf3f4677d0c8047ec97fd62195ca
Liraz-Benbenishti <lirazben6@gmail.com>
2020-09-22 03:38:56 +0300

    Delete hangman-ex3.4.2.py

--- hangman/hangman-unit3/hangman-ex3.4.2.py
+++
@@ -1,5 +0,0 @@
-user_string_input = input("Please enter a string: ")
-first_letter = user_string_input[0]
-user_string_input = first_letter + user_string_input[1:].replace(first_letter,"e")
-print(user_string_input)
-# ddar astronaut. pldase, stop drasing md!
